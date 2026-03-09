b54ee54837e9f6dfd99b682266509e81ced2e897
dev-prizrakk <am2005680@gmail.com>
2026-03-09 21:40:07 +0300

    Добавил реализацию Саб команд

--- src/main/java/ru/devprizrakk/voidbot/core/command/discord/CommandRegister.java
+++ src/main/java/ru/devprizrakk/voidbot/core/command/discord/CommandRegister.java
@@ -10,8 +10,10 @@ import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import org.jetbrains.annotations.NotNull;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
+
 import java.util.ArrayList;
 
 import java.util.List;
@@ -30,7 +32,9 @@ public class CommandRegister extends ListenerAdapter {
     public CommandRegister() {
         commands = new ArrayList<>();
     }
+
     public List<ICommand> commands;
+    public List<ISubCommand> subCommands;
 
 
     @Override
@@ -61,27 +65,70 @@ public class CommandRegister extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        for (final ICommand command : commands) {
-            if (command.getName().equals(event.getName())) {
+        for (ICommand command : commands) {
+            if (!command.getName().equals(event.getName())) {
+                continue;
+            }
+
+            String subName = event.getSubcommandName();
+
+            // 1) Обычная команда (не саб)
+            if (subName == null) {
                 if (!hasRequiredPermissions(event, command)) {
                     return;
                 }
                 try {
                     command.execute(event);
+                } catch (SQLException e) {
+                    Logger.getLogger().log(LogType.ERROR, "command", "", e);
                 }
-                catch (SQLException e) {
-                    Logger.getLogger().log(LogType.ERROR,"command", "", e);
-                }
+                return;
             }
+
+            // 2) Сабкоманда
+            List<ISubCommand> subs = command.getSubCommand();
+            if (subs == null || subs.isEmpty()) {
+                return; // root не поддерживает сабкоманды
+            }
+
+            for (ISubCommand sub : subs) {
+                if (!sub.getName().equals(subName)) {
+                    continue;
+                }
+
+                if (!hasRequiredPermissions(event, sub.getRequiredPermissions())) {
+                    return;
+                }
+
+                try {
+                    sub.execute(event);
+                } catch (SQLException e) {
+                    Logger.getLogger().log(LogType.ERROR, "sub-command", "", e);
+                }
+                return;
+            }
+
+            return; // subName пришел, но не найден в списке
         }
     }
+
+
     private void syncGuildCommands(Guild guild) {
         List<CommandData> built = new ArrayList<>();
 
         for (ICommand command : commands) {
             Logger.getLogger().log(LogType.INFO, "COMMAND_REGISTER", "Успешно зарегистрирована команда: " + command.getName());
             var data = Commands.slash(command.getName(), command.getDescription());
-            if (command.getOptions() != null && !command.getOptions().isEmpty()) {
+            if (command.getSubCommand() != null && !command.getSubCommand().isEmpty()) {
+                for (ISubCommand iSubCommand : command.getSubCommand()) {
+                    //subCommands.add(iSubCommand); // так как мы напрямую у CommandManager не уточняем команды на регистрацию мы их получаем когда регистрируем команды
+                    SubcommandData subcommandData = new SubcommandData(iSubCommand.getName(), iSubCommand.getDescription());
+                    if (iSubCommand.getOptions() != null && !iSubCommand.getOptions().isEmpty()) {
+                        subcommandData.addOptions(iSubCommand.getOptions());
+                    }
+                    data.addSubcommands(subcommandData);
+                }
+            } else if (command.getOptions() != null && !command.getOptions().isEmpty()) {
                 data.addOptions(command.getOptions());
             }
             built.add(data);
@@ -90,13 +137,16 @@ public class CommandRegister extends ListenerAdapter {
     }
 
 
-    public void add(final ICommand command) {
+    public void add(ICommand command) {
         commands.add(command);
     }
 
 
     public static boolean hasRequiredPermissions(SlashCommandInteractionEvent event, ICommand command) {
-        List<Permission> requiredPermissions = command.getRequiredPermissions();
+        return hasRequiredPermissions(event, command.getRequiredPermissions());
+    }
+
+    public static boolean hasRequiredPermissions(SlashCommandInteractionEvent event, List<Permission> requiredPermissions) {
         if (requiredPermissions == null || requiredPermissions.isEmpty()) {
             return true;  // Если прав не требуется
         }
@@ -106,5 +156,4 @@ public class CommandRegister extends ListenerAdapter {
         }
         return true;
     }
-
 }
--- src/main/java/ru/devprizrakk/voidbot/core/command/discord/ISubCommand.java
+++ src/main/java/ru/devprizrakk/voidbot/core/command/discord/ISubCommand.java
@@ -1,28 +1,18 @@
 package ru.devprizrakk.voidbot.core.command.discord;
 
-import java.sql.SQLException;
-
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public interface ICommand {
+public interface ISubCommand {
     String getName();
 
     String getDescription();
-
-    default List<OptionData> getOptions() {
-        return List.of();
-    }
-    CommandCategory getCategory();
-    default List<Permission> getRequiredPermissions() {
-        return List.of();
-    }
-    default  List<ISubCommand> getSubCommand() {
-        return List.of();
-    }
+    List<OptionData> getOptions();
+    List<Permission> getRequiredPermissions();
 
     void execute(final SlashCommandInteractionEvent event) throws SQLException;
 }
